b6d5963d8532b4ee1855ebdd572c9a0c1cf3d800
Karagulamos <karagulamos@gmail.com>
2020-03-02 18:40:31 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,44 @@
+// Author: Alexander Karagulamos
+// Remark: https://leetcode.com/submissions/detail/308692111/
+
+public class Solution 
+{
+  private const int Dimension = 8;
+  
+  private static (int Row, int Col)[] _directions;
+  
+  static Solution() => _directions = new (int, int)[]
+  {
+    (-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1)
+  };
+  
+  public IList<IList<int>> QueensAttacktheKing(int[][] queens, int[] king) 
+  {
+    var visited = queens.Aggregate(0L, (bvec, pos) => bvec | Mask(pos[0], pos[1]));
+        
+    var result = new List<IList<int>>();
+
+    foreach(var direction in _directions)
+    {
+      var row = direction.Row + king[0];
+      var col = direction.Col + king[1];
+      
+      while(row >= 0 && col >= 0 && row < Dimension && col < Dimension)
+      {
+        if(IsVisited(row, col))
+        {
+          result.Add(new[]{row, col}.ToList());
+          break;
+        }
+        
+        row += direction.Row;
+        col += direction.Col;
+      }
+    }
+
+    return result;
+
+    bool IsVisited(int row, int col) => (visited & Mask(row, col)) != 0;
+    long Mask(int row, int col) => 1L << row * Dimension + col;
+  }   
+}
